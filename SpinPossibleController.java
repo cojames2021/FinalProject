aaae745124c28472184bf8b4d342d2a4ad53095c
Connor James <cojames@carroll.edu>
2018-10-24 15:22:01 -0600

    controller additions

--- SpinPossibleController.java
+++ SpinPossibleController.java
@@ -1,10 +1,26 @@
 package spinPossible;
 
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
 public class SpinPossibleController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public SpinPossibleController(String title, int gameWindowX, int gameWindowY, int gameWindowWidth, int gameWindowHeight)
+	{
+		 	gameJFrame = new JFrame(title);
+	        gameJFrame.setSize(gameWindowWidth, gameWindowHeight);
+	        gameJFrame.setLocation(gameWindowX, gameWindowY);
+	        gameJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        gameContentPane = gameJFrame.getContentPane();
+	        gameContentPane.setLayout(null); // not need layout, will use absolute system
+	        gameContentPane.setBackground(Color.blue);
+	        gameJFrame.setVisible(true);
 	}
 	
 	public void createGrid(String filename) {
